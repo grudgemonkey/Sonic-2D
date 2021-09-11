ceee223c5a75dd32598c1239449f15bbd140f615
grudgemonkey <77580981+grudgemonkey@users.noreply.github.com>
2021-09-10 20:27:43 -0400

    various things

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 enum {
 	ON_GROUND,
 	IN_AIR,
-	ROLLING
+	JUMPING,
+	ROLLING,
+	GRINDING
 }
 
 var move_speed = 40
@@ -12,6 +14,7 @@ var jump_force = 1500
 var acc = 10
 var max_speed = 1000
 var run_threshold = 950
+var frc = 300
 
 var state = IN_AIR
 
@@ -21,57 +24,57 @@ var input_vector : Vector2
 func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	 
-	
 	match state:
+		JUMPING:
+			$AnimatedSprite.play("Spin")
+			velocity.y += grv
+			if $RayCast2D.is_colliding():
+				state = ON_GROUND
+		GRINDING:
+			$AnimatedSprite.play("Idle")
+			velocity.x = move_speed
+			velocity.y = 0
+			if Input.is_action_just_pressed("jump"):
+				velocity.y -= jump_force
+				state = JUMPING
 		IN_AIR:
-			if input_vector.x != 0:
-				velocity.x = input_vector.x * move_speed
-				if input_vector.x < 0:
-					$AnimatedSprite.flip_h = true
-				else:
-					$AnimatedSprite.flip_h = false
-			
 			velocity.y += grv
 			if $RayCast2D.is_colliding():
 				state = ON_GROUND
 		ROLLING:
-			if Input.is_action_just_pressed("jump"):
-				velocity.y = -jump_force
-				if $RayCast2D.is_colliding():
-					state = ON_GROUND
-			else:
-				velocity.y += grv
 			$AnimatedSprite.play("Spin")
+			move_speed -= frc
+			if move_speed == 0:
+				state = ON_GROUND
+			if $RayCast2D.is_colliding() == false:
+				state = IN_AIR
 		ON_GROUND:
 			if input_vector.x != 0:
-				move_speed += acc
-				velocity.x = input_vector.x * move_speed
-				if move_speed >= run_threshold:
-					$AnimatedSprite.play("Run")
-				else:
-					$AnimatedSprite.play("Walk")
-				if move_speed >= max_speed:
-					move_speed = max_speed
+				$AnimatedSprite.play("Walk")
 				if input_vector.x < 0:
 					$AnimatedSprite.flip_h = true
 				else:
 					$AnimatedSprite.flip_h = false
-				
-				if Input.is_action_just_pressed("down"):
-					state = ROLLING
+				move_speed += acc
+				velocity.x = input_vector.x * move_speed
 			else:
-				move_speed = 30
-				velocity.x = 0
 				$AnimatedSprite.play("Idle")
-			
-			velocity.y = 0
+				move_speed = 40
+				velocity.x = 0
 			
 			if Input.is_action_just_pressed("jump"):
-				state = IN_AIR
 				velocity.y = -jump_force
-				$AnimatedSprite.play("Spin")
-				$JumpSound.play()
+				state = JUMPING
 			
 			if $RayCast2D.is_colliding() == false:
 				state = IN_AIR
+			
+			if Input.is_action_just_pressed("down"):
+				state = ROLLING
+	
 	move_and_slide(velocity)
+
+
+func _on_GrindRail_body_entered(body):
+	state = GRINDING
+	print("is geinding")
